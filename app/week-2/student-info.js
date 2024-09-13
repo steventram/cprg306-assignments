e2fb5c5879a5d275527438a274098aac099b148f
steventram <steven.tram@edu.sait.ca>
2024-09-13 09:56:16 -0600

    add StudentInfo component, links, and displays
    I added the StudentInfo component and implemented it into page 2. I also created a link to my github repo and updated the displays with links between the pages

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Name: Steven Tram</p>
+            <Link href="https://github.com/steventram">Steven Tram GitHub</Link>
+        </div>
+    );
+}
