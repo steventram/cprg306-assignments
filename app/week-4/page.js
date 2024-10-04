c6c5e766138f9a9a586c84191f65d00bd4cf798e
steventram <steven.tram@edu.sait.ca>
2024-10-04 11:59:16 -0600

    styled NewItem and implemented disabled feature

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,7 +2,7 @@ import NewItem from "./new-item";
 
 export default function Page4() {
   return (
-    <main>
+    <main className="bg-slate-600 text-center flex-auto">
       <NewItem />
     </main>
   );
