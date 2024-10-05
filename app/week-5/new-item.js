ccebf6ca725932d97c5a9476be4e202366cc14be
steventram <steven.tram@edu.sait.ca>
2024-10-04 18:19:22 -0600

    add handleSubmit, create useStates, add form with name and category fields
    Created handleSubmit function. Then created fields for name and category. Created form that included all fields along with submit button that brings up an alert box that displays current state of name, quantity, and category. State changes are also handled by onChange functions

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 export default function NewItem() {
   const [quantity, setQuantity] = useState(1);
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("produce");
 
   const increment = () => {
     if (quantity < 20) {
@@ -26,25 +28,87 @@ export default function NewItem() {
   if (quantity == 20) {
     incrementDisabled = true;
   }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let item = {
+      itemName: name,
+      itemQuantity: quantity,
+      itemCategory: category,
+    };
+
+    setName("");
+    setQuantity(1);
+    setCategory("produce");
+
+    alert(`
+      Name: ${item.itemName}
+      Quantity: ${item.itemQuantity}
+      Category: ${item.itemCategory}
+      `);
+  };
+
+  const handleNameChange = (event) => setName(event.target.value);
+  const handleQuantityChange = (event) => setQuantity(event.target.value);
+  const handleCategoryChange = (event) => setCategory(event.target.value);
+
   return (
-    <main className="bg-gray-900-200">
-      <p className="pt-2 text-white text-2xl">Quantity: {quantity}</p>
-      <div className="p-3 space-x-2">
-        <button
-          onClick={decrement}
-          className="w-28 font-bold bg-red-400 text-white hover:bg-red-700  disabled:bg-slate-400 active:bg-red-300 rounded-xl"
-          disabled={decrementDisabled}
-        >
-          -
-        </button>
-        <button
-          onClick={increment}
-          className="w-28 font-bold bg-green-400 text-white hover:bg-green-700  disabled:bg-slate-400 active:bg-green-300 rounded-xl"
-          disabled={incrementDisabled}
-        >
-          +
-        </button>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>item name</label>
+        <input type="text" onChange={handleNameChange} value={name} required />
       </div>
-    </main>
+
+      <div>
+        <p
+          className="pt-2 text-white text-2xl"
+          onChange={handleQuantityChange}
+          value={quantity}
+        >
+          Quantity: {quantity}
+        </p>
+        <div className="p-3 space-x-2">
+          <button
+            type="button"
+            onClick={decrement}
+            className="w-28 font-bold bg-red-400 text-white hover:bg-red-700  disabled:bg-slate-400 active:bg-red-300 rounded-xl"
+            disabled={decrementDisabled}
+          >
+            -
+          </button>
+          <button
+            type="button"
+            onClick={increment}
+            className="w-28 font-bold bg-green-400 text-white hover:bg-green-700  disabled:bg-slate-400 active:bg-green-300 rounded-xl"
+            disabled={incrementDisabled}
+          >
+            +
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <label>produce category</label>
+        <select onChange={handleCategoryChange} value={category}>
+          <option selected value="produce">
+            produce
+          </option>
+          <option value="dairy">dairy</option>
+          <option value="bakery">bakery</option>
+          <option value="meat">meat</option>
+          <option value="frozen foods">frozen foods</option>
+          <option value="canned goods">canned goods</option>
+          <option value="dry goods">dry goods</option>
+          <option value="beverages">beverages</option>
+          <option value="snacks">snacks</option>
+          <option value="household">household</option>
+          <option value="other">other</option>
+        </select>
+      </div>
+      <div>
+        <button className="border-slate-800 border-2">Add to List</button>
+      </div>
+    </form>
   );
 }
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -2,7 +2,7 @@ import NewItem from "./new-item";
 
 export default function Page5() {
   return (
-    <main className="bg-slate-600 text-center flex-auto">
+    <main className="bg-slate-600 text-center  flex-auto p-3">
       <NewItem />
     </main>
   );
